e925058782cde21d2c88e2fd1d7c482e53286b13
KuranV <vladimir.kuran.s@gyarab.cz>
2026-01-23 10:17:43 +0100

    Add currency conversion functionality for EUR to CZK

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -5,3 +5,19 @@ print(r.text)
 print(r.status_code)
 lines = r.text.split('\n')
 print(lines[0])
+
+line_euro = ""
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+rate_str = line_euro.split('|')[-1].replace(',', '.')
+rate = float(rate_str)
+
+print(f"Current EUR/CZK rate: {rate}")
+
+value_in = float(input("Enter amount in EUR: "))
+
+vlaue_out = value_in * rate
+print(f"Your's {value_in} EUR is {vlaue_out} CZK at the current rate.")
