f35ede7e6f0494f49ef0cd2c26b971e6b031c9d0
KuranV <vladimir.kuran.s@gyarab.cz>
2026-01-23 10:05:47 +0100

    typo

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,6 +1,6 @@
 import httpx
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=21.01.2026')
+r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
 print(r.text)
 print(r.status_code)
 lines = r.text.split('\n')
