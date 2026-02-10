54f7245ad4d998d3ca38df5a5fbddfa8644d8f06
KuranV <vladimir.kuran.s@gyarab.cz>
2026-02-10 18:44:08 +0100

    .

---
+++ microsite/soubory/odborky/stareStranky.php
@@ -0,0 +1,229 @@
+
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-2RVEBQNJNL"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() { dataLayer.push(arguments); }
+		gtag('js', new Date());
+
+		gtag('config', 'G-2RVEBQNJNL');
+	</script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Turistický oddíl - 49. pionýrská skupina Kočovníci z Prahy 6" />
+	<meta name="keywords" content="turistika, oddíl, výlet, tábor, letní tábor, volný čas, děti, pionýr, praha 6" />
+	<title>
+		Turistický oddíl Kočovníci
+	</title>
+	<link rel="stylesheet" type="text/css" href="kocovnici.css">
+	<!-- 		lightbox 2 -->
+	<!-- prototype.js bourá načítání facebooku, zakomentováno - TODO: vyřešit lépe 
+<script type="text/javascript" src="fotky/lightbox/js/prototype.js"></script>
+-->
+	<script type="text/javascript" src="fotky/lightbox/js/scriptaculous.js?load=effects"></script>
+	<script type="text/javascript" src="fotky/lightbox/js/lightbox.js"></script>
+
+	<link rel="stylesheet" href="fotky/lightbox/css/lightbox.css" type="text/css" media="screen" />
+
+</head>
+
+<body>
+
+	<div id="fb-root"></div>
+	<script>(function (d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v2.4";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
+	<div id="container">
+
+		<div id="header">
+			<table>
+				<tr>
+					<td align="left" width="210px">Na naši činnost přispívají</td>
+					<td align="left">
+						<a href="http://www.praha6.cz" title="Praha 6" target="_new">MČ Praha 6</a>
+						<a href="http://magistrat.praha-mesto.cz/" title="Magistrát Hl. m. Prahy"
+							target="_new">Magistrát Hl. m. Prahy</a>
+						<a href="http://www.msmt.cz/" title="Ministerstvo školství, mládeže a tělovýchovy"
+							target="_new">MŠMT</a>
+					</td>
+					<td align="right"><img src="images/pionyr.gif" align="absmiddle"></td>
+					<td width="40px"><a href="http://www.pionyr.cz" title="Přejít na stránky Pionýra"
+							target="_new">Pionýr</a></td>
+				</tr>
+			</table>
+		</div>
+
+		<div id="main">
+
+			<div id="menu">
+				<table>
+					<tr>
+						<td><a href="index.php?pg=novinky" title="Aktuality z oddílu">Novinky</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=onas" title="Kdo jsme a co děláme">O nás</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=druziny" title="Přehled schůzek, kontakty na vedoucí">Členové</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=plan" title="Co se chystá">Plán akcí</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=tabory" title="Letní a zimní tábory">Tábory</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=fotky" title="Fotky z oddílových akcí">Fotky</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=odborky" title="Užitečné znalosti">Odborky</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=historie" title="Historie oddílu">Historie</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=typko" title="Archiv oddílového časopisu">Týpko</a></td>
+					<tr>
+					<tr>
+						<td><a href="index.php?pg=diskuze" title="Přispějte svým názorem">Diskuse</a></td>
+					<tr>
+
+				</table>
+
+				<p style="margin-top:150px;">
+					<a href="http://www.kocovnici.rajce.idnes.cz/" target="blank"><img src="images/rajce.jpg"
+							width="120" height="42" alt="Kočovnická galerie na rajce.net" border="0"></a>
+				</p>
+
+			</div>
+
+			<div id="content">
+
+
+				<div id="content_center">
+
+					<div class="nadpis_greenbox">Odborky</div>
+<div class="blok_greenbox"></div>
+
+<div class="nadpis_orange">Přehled a pravidla</div>
+<div class="blok_orange">
+
+	<p>Odborky představují soubor znalostí, které by měl každý člen oddílu nabýt před pasováním na instruktora:
+	<p>
+
+		<a href="odborky/odborky_podminky_v2.pdf" class="dokument">pravidla plnění a přehled všech odborek</a>
+
+	<p>
+		Každá odborka má čtyři stupně (1. - nejlehčí, 4. - nejtěžší). Jednotlivé stupně lze plnit postupně, nicméně kdo
+		chce splnit např. 3. stupeň,
+		musí již mít 1. a 2. stupeň téže odborky (případně splnit 1.-3. najednou).
+	</p>
+
+	<p>
+		Splnění stupně odborky je odměněno získáním odznáčku odpovídajícího stupně a 3 body do bodování.
+	</p>
+
+	<p>
+		Podmínkou pro šátkování je získání alespoň 5 stupňů libovolných odborek, tj. např.:
+	<ul>
+		<li>prvních stupňů různých odborek</li>
+		<li>jednoho třetího stupně jedné odborky a jednoho druhého stupně jiné odborky</li>
+	</ul>
+	</p>
+
+	<p>
+		Podmínkou pro pasování na instruktora je splnění všech třetích stupňů a jednoho vybraného čtvrtého stupně.
+	</p>
+
+</div>
+
+<div class="nadpis_orange">Listy jednotlivých odborek</div>
+<div class="blok_orange">
+	<a href="odborky/hvezdar.pdf" class="dokument">Hvězdář</a><br>
+	<a href="odborky/kuchar.pdf" class="dokument">Kuchař</a><br>
+	<a href="odborky/morse.pdf" class="dokument">Morse</a><br>
+	<a href="odborky/prirodovedec.pdf" class="dokument">Přírodovědec</a><br>
+	<a href="odborky/sportovec.pdf" class="dokument">Sportovec</a><br>
+	<a href="odborky/tabornik.pdf" class="dokument">Táborník</a><br>
+	<a href="odborky/topograf.pdf" class="dokument">Topograf</a><br>
+	<a href="odborky/uzlar.pdf" class="dokument">Uzlař</a><br>
+	<a href="odborky/zdravotnik.pdf" class="dokument">Zdravotník</a><br>
+</div>
+
+<!-- <div class="nadpis_orange">Staré listy odborek</div>
+<div class="blok_orange">
+  <a href="odborky/botanik_puvodni.pdf" class="dokument">Botanik</a><br>
+  <a href="odborky/hvezdar_puvodni.pdf" class="dokument">Hvězdář</a><br>
+  <a href="odborky/kuchar_puvodni.pdf" class="dokument">Kuchař</a><br>
+  <a href="odborky/morse_puvodni.pdf" class="dokument">Morse</a><br>
+  <a href="odborky/ohnar_puvodni.pdf" class="dokument">Ohňař</a><br>
+  <a href="odborky/sportovec_puvodni.pdf" class="dokument">Sportovec</a><br>
+  <a href="odborky/stopar_puvodni.pdf" class="dokument">Stopař</a><br>
+  <a href="odborky/topograf_puvodni.pdf" class="dokument">Topograf</a><br>
+  <a href="odborky/uzlar_puvodni.pdf" class="dokument">Uzlař</a><br>
+  <a href="odborky/zdravotnik_puvodni.pdf" class="dokument">Zdravotník</a><br>
+</div> -->				</div>
+
+				<div id="content_right">
+					<div class="right_nadpis">Kontakt</div>
+					<div class="right_div">
+						<b>Pionýr, z.s.<br>
+							49. pionýrská skupina<br>
+							Kočovníci</b><br>
+						IČO: 66005752<br>
+						Účet: 2700230041/2010<br>
+						Dat. schránka: s4s5ecn<br>
+						<hr>
+						Klubovna
+						&nbsp;&nbsp;&nbsp;<a href="http://www.mapy.cz/#st=s@sss=1@ssq=id%3Apremise%201447365@@"
+							title="Odkaz na mapy.cz">mapa</a><br>
+						Nad Panenskou 5<br>
+						Praha 6<br>
+						Tel.: 731 261 239<br>
+						<hr>
+						<a href="mailto:info@kocovnici.cz">info@kocovnici.cz</a><br>
+					</div>
+
+					<div class="right_nadpis">Ke stažení</div>
+					<div class="right_div">
+						<a href="dokumenty/prihlaska.pdf" class="dokument" title="Přihláška do oddílu">Přihláška do
+							oddílu</a><br>
+						<hr>
+						Seznamy věcí na:<br>
+						<a href="dokumenty/DSV_ven.pdf" class="dokument"
+							title="Doporučený seznam věcí na vícedenní výlet ven">Výlet ven</a><br>
+						<a href="dokumenty/DSV_budova.pdf" class="dokument"
+							title="Doporučený seznam věcí na vícedenní výlet do budovy">Výlet do budovy</a><br>
+						<a href="dokumenty/DSV_jednodenak.pdf" class="dokument"
+							title="Doporučený seznam věcí na jednodenní výlet">Jednodenní výlet</a><br>
+						<a href="dokumenty/DSV_tabor.pdf" class="dokument"
+							title="Doporučený seznam věcí na letní tábor">Letní tábor</a><br>
+						<hr>
+						<a href="dokumenty/zpevnik.pdf" class="dokument" title="Oddílový zpěvník">Zpěvník</a><br>
+					</div>
+
+										<div class="fb-like" data-href="https://facebook.com/kocovnici" data-width="40"
+						data-layout="box_count" data-action="like" data-show-faces="false" data-share="true"></div>
+
+				</div>
+
+			</div>
+		</div>
+
+		<div id="footer">
+		</div>
+
+	</div>
+	</div>
+</body>
+
+</html>
